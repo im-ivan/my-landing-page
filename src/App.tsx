a5e90d723d465be782f5924ba56213b920973806
im-ivan <ivanhfc00@gmail.com>
2025-10-24 11:45:33 -0300

    ft: navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Navbar } from "./components/Navbar";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Start project</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import { navItems } from "../assets/constants/index.tsx";
+import { Menu, X } from "lucide-react";
+import logo from "../assets/img/logo.png";
+import { useState } from "react";
+
+export const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const toggleNavbar = (): void => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80 bg-yellow-600">
+      <div className="container px4 mx-auto relative text-sm">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-20 m-3" src={logo} alt="Saliri logo" />
+            <span className="text-xl tracking-tight font-sans">Saliri</span>
+          </div>
+          <ul className="hidden lg:flex ml-14 space-x-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="" className="py-2 px-3 border rounded-md">
+              Sign in
+            </a>
+            <a
+              href=""
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create account
+            </a>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="" className="py-2 px-3 border rounded-md">
+                Sign in
+              </a>
+              <a
+                href=""
+                className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              >
+                Create account
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
