a3d9f5beba9cab8cda274d27de55bf6110311f59
im-ivan <ivanhfc00@gmail.com>
2025-10-24 15:54:59 -0300

    ft: feature section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FeatureSection } from "./components/FeatureSection";
 import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
 function App() {
@@ -6,6 +7,9 @@ function App() {
       <Navbar />
       <div className="hero-container max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+      </div>
+      <div className="feature-container max-w-7xl mx-auto pt-20 px-6">
+        <FeatureSection />
       </div>
     </>
   );
