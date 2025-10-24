106397b3d9636e13adfe92da2add027ca093261b
im-ivan <ivanhfc00@gmail.com>
2025-10-24 17:34:10 -0300

    ft: footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FeatureSection } from "./components/FeatureSection";
+import { Footer } from "./components/Footer";
 import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
 import { PricingSection } from "./components/PricingSection";
@@ -22,6 +23,9 @@ function App() {
       </div>
       <div className="testimonials-container max-w-7xl mx-auto pt-20 px-6">
         <TestimonialsSection />
+      </div>
+      <div className="footer-container max-w-7xl mx-auto pt-20 px-6">
+        <Footer />
       </div>
     </>
   );
